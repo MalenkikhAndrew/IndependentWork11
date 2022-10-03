da0ea49facdf02b3d4a9f09dc87238f02f6f1966
User <User@DESKTOP-A50L8A1>
2022-10-03 17:17:29 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork11/LinUr.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork11
+{
+    struct LinUr
+    {
+        double k;
+        double b;
+
+        public LinUr(double k, double b) 
+        {
+            this.k = k; 
+            this.b = b;
+
+        }
+        public string Root() 
+        {
+            double x = -b / k;
+            return $"X={x}";
+
+        }
+    }
+}
---
+++ IndependentWork11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести значения k,b для линейного уравнения 0=kx+b");
+            Console.Write("k=");
+            double k=Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            double b=Convert.ToDouble(Console.ReadLine());
+
+            LinUr linur=new LinUr(k,b);
+            Console.WriteLine(linur.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
